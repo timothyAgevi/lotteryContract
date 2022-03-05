8294ce8ff62daea5bf40cba2f4ec97b6a8366aff
timothyAgevi <timothyagevi@gmail.com>
2022-03-05 23:01:04 +0300

    add comments on test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -4,3 +4,6 @@ const ganache= require('ganache-cli');// local test network
 const Web3 =require('web3');// constructor function
 const web3 =new Web3(ganache.provider());//instance of web3
 const {abi,bytecode}= require('../compile')
+
+
+//2 variables:1 instance of contrACT, 2. LIST OF ALL ganache unlocked a/c for us
