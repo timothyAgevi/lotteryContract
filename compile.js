28450163b2b6c6d7ef6f55aaff556997567ab147
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 00:59:31 +0300

    update compile.js

--- compile.js
+++ compile.js
@@ -1,45 +1,40 @@
-const path= require('path');
-const fs =require('fs');
-const solc=require('solc');
+const path = require('path')
+const fs = require ('fs')
 
-//find path
-const lotteryPath= path.resolve(__dirname,'contracts','Lottery.sol');
+const solc = require('solc');
+// We will have cross platform compatipility. 
+//__dirname it used by node to take you to the root directory.
+const lotteryPath = path.resolve(__dirname, 'contracts','Lottery.sol');
+ //Reading the contents of the file.
+ const source = fs.readFileSync(lotteryPath,'utf8');
 
-//read content of file
-const source =fs.readFileSync(lotteryPath,'utf8');
+// // number of sources we are compiling is one.
+// module.exports = solc.compile(source,1).contracts[':Inbox'];
 
-var input = {
-    language: 'Solidity',
+
+const input = {
+    language: "Solidity",
     sources: {
-        'Lottery.sol' : {
-            content: source
-        }
+      "Lottery.sol": {
+        content: source,
+      },
     },
     settings: {
-        outputSelection: {
-            '*': {
-                '*': [ "*" ]
-            }
-        }
-    }
-};
+      outputSelection: { 
+        "*": {
+          "*": ["*"],
+        },
+      },
+    },
+  };
+  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+  // `output` here contains the JSON output as specified in the documentation
+  var outputContracts = output.contracts['Lottery.sol']['Lottery']
 
+  // exports ABI interface
+  module.exports.abi = outputContracts.abi;
+  
+  // exports bytecode from smart contract
+  module.exports.bytecode = outputContracts.evm.bytecode.object;
 
-
-
-//compile  statement
-//  module.exports = solc.compile(source,1).contracts[':Lottery'];
-
-
-
-
-const output =JSON.parse(solc.compile(JSON.stringify(input)));
-
-//output here contains JSON output as specified in documentation
-var outputContracts= output.contracts['Lottery.sol']['Lottery']
-
-//export ABI interface
-module.exports.abi = outputContracts.abi
-
-// export bytecode
-module.exports.bytecode = outputContracts.evm.bytecode.object;
+  
