80b42e66a5a8189e0a713583786388d103857fd1
timothyAgevi <timothyagevi@gmail.com>
2022-03-05 22:55:36 +0300

    update

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,6 +1,6 @@
 const { strictEqual } = require('assert');
 const assert = require('assert');
-const ganache= require('ganache-cli');
+const ganache= require('ganache-cli');// local test network
 const Web3 =require('web3');// constructor function
 const web3 =new Web3(ganache.provider());//instance of web3
 const {abi,bytecode}= require('../compile')
