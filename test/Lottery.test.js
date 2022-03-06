9f134f8c79564460939adb48e9ddf8d75ca19e1c
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 14:07:56 +0300

    update

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,6 +1,6 @@
 
-const assert =require('assert');
-const ganache= require('ganache-cli');// local test network
+const assert =require('Assert');
+const ganache= require('Ganache-cli');// local test network
 const Web3 =require('web3');// constructor function
 const web3 =new Web3(ganache.provider());//instance of web3
 const {abi,bytecode}= require('../compile')
@@ -14,11 +14,25 @@ beforeEach( async()=>{
     accounts=await web3.eth.getAccounts();
     lottery= await  web3.eth.Contract(JSON.parse(abi))
     .deploy({data:bytecode}
-        .send({from:accounts[0],gas:"1000000"}))
+    .send({from:accounts[0],gas:"1000000"}))
 })
 describe('Lottery Contract',()=>{
     it('deploys a contract',()=>{
         assert.ok(lottery.options.address)
     })
+    it('allows one account to enter',async()=>{
+        await lottery.methods.enter.send({
+            from:accounts[0],
+            value:web3.utils.toWei('0.02','ether')
+        });
+    })
+    const players =await lottery.methods.getPlayers().call({
+        from:accounts[0]
+    });
+    //only element in players array should be address atzero
+    // only 1 element in array
+assert.equal(accounts[0],players[0])
+    //corect address within array
+    assert.equal(1,players.length);
 })
 
